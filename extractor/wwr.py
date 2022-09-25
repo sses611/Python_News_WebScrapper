2a3f5b754a6b407df663b4d0c7af91339dec1dc4
Dans <32377978+sses611@users.noreply.github.com>
2022-09-25 16:37:49 +0000

    네이버 부동산 뉴스 스크랩핑 작업

--- extractor/wwr.py
+++ extractor/wwr.py
@@ -1,5 +1,5 @@
-# from requests import get
-# from bs4 import BeautifulSoup
+from requests import get
+from bs4 import BeautifulSoup
 
 # def extract_wwr_jobs(keyword):
 #   base_url = "https://weworkremotely.com/remote-jobs/search?utf8=%E2%9C%93&term="
