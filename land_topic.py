32c91d6d66279b2dcf1e5295d60ea4c70ff1fe8a
Dans <32377978+sses611@users.noreply.github.com>
2022-09-25 16:42:55 +0000

    add

---
+++ land_topic.py
@@ -0,0 +1,62 @@
+from requests import get
+from bs4 import BeautifulSoup
+
+Naver_url = "https://land.naver.com/news/field.naver"
+Daum_url = ""
+ 
+response = get(Naver_url) 
+
+if response.status_code != 200:
+  print("Can't request website")
+else:
+  soup = BeautifulSoup(response.text, "html.parser")
+  news = soup.find("div", class_="section_headline")
+  news_post = news.find_all("li", reclusive=False)
+
+
+  def info_search():
+    article_info = article.find("dd")
+    text = article_info.text
+    press = article_info.find("span", class_="writing").text
+    date = article_info.find("span", class_="date").text
+    print(text, "\n", press, "\n", date)
+  
+   
+
+  for article in news_post: 
+    # photos =  article.select("img")
+    # photo_alt = photos['src']
+    # print(photo_alt)
+    info_search() 
+    # else:
+    #   photo = "No thumnail"
+    #   print(photo)
+    #   info_search()
+
+
+
+
+
+  # for photo in photos:
+  #   photo_link = photo.find_all('a')
+  #     # company, kind, region = anchor.find_all('span', class_="company")
+  #     # title = anchor.find('span', class_= 'title')
+  #   print(photo_link)
+  #   print("//////////////////","\n")
+  # #    texts = title[1]
+
+  #     subTitle = acricle.find_all('dd')
+  #     press = subTitle[0]
+  #     date  = subTitle[1]      
+  #     print(photos, "\n")
+  #     print("///////////////////\n")
+  #     # date = news_list.find_all("span", class_='date')
+    # # link = news_list['href']
+    # # img = news_list['img']
+    
+    # # news_data = {
+    # #   date : date,
+    # #   link : link,
+    # #   img : img
+    # # }
+    # print(news_list, "\n//////////////////\n")
---
+++ test.py
@@ -0,0 +1,34 @@
+from requests import get
+from bs4 import BeautifulSoup
+
+def extract_wwr_jobs(keyword):
+  base_url = "https://weworkremotely.com/remote-jobs/search?utf8=%E2%9C%93&term="
+  
+  response = get(f"{base_url}{keyword}")
+  if response.status_code != 200:
+    print("Can't request website")
+  else:
+    soup = BeautifulSoup(response.text, "html.parser")
+    jobs = soup.find_all('section', class_="jobs") #class_로 해야 찾을 수 있다.
+    results = []
+    for job_section in jobs:
+      job_post = job_section.find_all('li')
+      job_post.pop(-1) 
+      
+      for post in job_post:
+        anchors = post.find_all('a')
+        anchor = anchors[1]
+        link = anchor['href']
+        company, kind, region = anchor.find_all('span', class_="company")
+        title = anchor.find('span', class_= 'title')
+      
+        job_data = {
+          "company" : company.string,
+          "kind" : kind.string,
+          "region" : region.string,
+          "position" : title.string,
+          "link" : f"https://weworkremotely.com/{link}"
+        }
+        results.append(job_data)
+  return results
+    
