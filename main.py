17236318d577f7d20c9da12d26ef4035c11bc3dc
Dans <32377978+sses611@users.noreply.github.com>
2022-09-25 16:36:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# from requests import get
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+
+# # from bs4 import BeautifulSoup
+# # from extractor.wwr import extract_wwr_jobs
+ 
+# options = Options()
+# options.add_argument("--no-sandbox")
+# options.add_argument("--disable-dev-shm-usage")
+
+# # jobs = extract_wwr_jobs("python")
+# # print(jobs)
+
+# browser = webdriver.Chrome(options=options)
+# browser.get("https://www.indeed.com/jobs?q="
+
+# print(browser.page_source)
+
+
+# # if response.status_code != 200
+# #   print("Can't response site")
+# # else:
+# #   print(response.text)
+
+
+
