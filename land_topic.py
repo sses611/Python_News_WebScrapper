f417c96d8436d6112055b959a541fd9b30f71b45
Dans <32377978+sses611@users.noreply.github.com>
2022-09-26 16:22:41 +0000

    web flask 작업 준비

--- land_topic.py
+++ land_topic.py
@@ -1,62 +1,19 @@
 from requests import get
-from bs4 import BeautifulSoup, requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+from extrator.moneytoday import search_news
 
-Naver_url = "https://land.naver.com/news/field.naver"
-Daum_url = ""
- 
-response = get(Naver_url) 
+TODAY = datetime.now().date()
 
-if response.status_code != 200:
-  print("Can't request website")
-else:
-  soup = BeautifulSoup(response.text, "html.parser")
-  news = soup.find("div", class_="section_headline")
-  news_post = news.find_all("li", reclusive=False)
+MT = 'https://news.mt.co.kr/newsList.html?pDepth1=estate&pDepth2=Etotal' # 머니투데이
+MK = 'https://www.mk.co.kr/news/realestate/' #매일
 
+MoneyToday = search_news('머니투데이', MT)
+# MeailK = search_new(MeailK, MK)
 
-  def info_search():
-    article_info = article.find("dd")
-    text = article_info.text
-    press = article_info.find("span", class_="writing").text
-    date = article_info.find("span", class_="date").text
-    print(text, "\n", press, "\n", date)
-  
-   
+file = open(f"{TODAY}.csv", 'w')
+file.write("center, image, title, link ,press, regidate\n")
 
-  for article in news_post: 
-    # photos =  article.select("img")
-    # photo_alt = photos['src']
-    # print(photo_alt)
-    info_search() 
-    # else:
-    #   photo = "No thumnail"
-    #   print(photo)
-    #   info_search()
-
-
-
-
-
-  # for photo in photos:
-  #   photo_link = photo.find_all('a')
-  #     # company, kind, region = anchor.find_all('span', class_="company")
-  #     # title = anchor.find('span', class_= 'title')
-  #   print(photo_link)
-  #   print("//////////////////","\n")
-  # #    texts = title[1]
-
-  #     subTitle = acricle.find_all('dd')
-  #     press = subTitle[0]
-  #     date  = subTitle[1]      
-  #     print(photos, "\n")
-  #     print("///////////////////\n")
-  #     # date = news_list.find_all("span", class_='date')
-    # # link = news_list['href']
-    # # img = news_list['img']
-    
-    # # news_data = {
-    # #   date : date,
-    # #   link : link,
-    # #   img : img
-    # # }
-    # print(news_list, "\n//////////////////\n")
+for news in MoneyToday:
+  file.write(f"{news['center']}, {news['image']}, {news['title']}, {news['link']}, {news['press']}, {news['regidate']}\n")
+file.close()
--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from requests import get
 from bs4 import BeautifulSoup
 from datetime import datetime
 from extrator.moneytoday import search_news
+# from extrator.MKnews 
 
 TODAY = datetime.now().date()
 
-MT = 'https://news.mt.co.kr/newsList.html?pDepth1=estate&pDepth2=Etotal'
-MK = 'https://www.mk.co.kr/news/realestate/'
+MT = 'https://news.mt.co.kr/newsList.html?pDepth1=estate&pDepth2=Etotal' # 머니투데이
+MK = 'https://www.mk.co.kr/news/realestate/' #매일
 
 MoneyToday = search_news('머니투데이', MT)
 # MeailK = search_new(MeailK, MK)
@@ -17,7 +18,3 @@ file.write("center, image, title, link ,press, regidate\n")
 for news in MoneyToday:
   file.write(f"{news['center']}, {news['image']}, {news['title']}, {news['link']}, {news['press']}, {news['regidate']}\n")
 file.close()
-
-
-
-
